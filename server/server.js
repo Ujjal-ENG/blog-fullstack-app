6dd3ad3317d46aec9a0fe00c2594d4819d4b1df1
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-22 02:02:26 +0600

    server is running perfectly

--- server/server.js
+++ server/server.js
@@ -1,6 +1,25 @@
-// dependencies
-const http = require('http');
-// app object - module scaffolding
-const app = {};
-// configuration
-app.config = {};
+/* eslint-disable indent */
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// rest object
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// routes
+app.get('/', (req, res) => {
+    res.status(200).json({
+        msg: 'hello from server',
+    });
+});
+
+// listen
+
+app.listen(8080, () => {
+    console.log('Server is runing at 8080 port');
+});
