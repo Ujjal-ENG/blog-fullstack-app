26a571e099cac44f81442f99d31a38b7b1ee43e3
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-22 02:48:36 +0600

    done the make our first user model and now i will create a userControllers and Route

---
+++ server/model/usersModel.js
@@ -0,0 +1,25 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable indent */
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema(
+    {
+        userName: {
+            type: String,
+            required: [true, 'username is required'],
+        },
+        email: {
+            type: String,
+            required: [true, 'email is required'],
+        },
+        password: {
+            type: String,
+            required: [true, 'password is required'],
+        },
+    },
+    { timesStamps: true }
+);
+
+const userModel = mongoose.model('User', UserSchema);
+
+module.exports = userModel;
