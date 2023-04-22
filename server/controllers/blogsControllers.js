66fc038a24ff93ae53314c6c8ed397867ac35ff6
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-22 17:05:14 +0600

    done the add blogs controller and it worked properly

--- server/controllers/blogsControllers.js
+++ server/controllers/blogsControllers.js
@@ -3,8 +3,29 @@ const mongoose = require('mongoose');
 
 const blogModel = require('../model/blogsModel');
 // get all blogs
-exports.getAllBlogs = (req, res) => {
-    res.send('get all blogs');
+exports.getAllBlogs = async (req, res) => {
+    try {
+        const blogs = await blogModel.find();
+        if (!blogs) {
+            res.status(200).json({
+                message: 'No blogs found!!',
+                success: true,
+                results: blogs.length,
+                data: blogs,
+            });
+        }
+        res.status(200).json({
+            message: 'Blogs are founded',
+            success: true,
+            results: blogs.length,
+            data: blogs,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error,
+            success: false,
+        });
+    }
 };
 // create blog
 exports.createBlog = (req, res) => {
