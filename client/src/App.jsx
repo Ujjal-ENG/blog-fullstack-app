828648adf7d4587cbd9fc1456e164c646d7dad0a
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-23 03:28:31 +0600

    now all is word perfectly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/jsx-indent */
 import { Outlet } from 'react-router-dom';
+import Footer from './components/layouts/Footer';
 import Navbar from './components/layouts/Navbar';
 
 function App() {
     return (
         <>
             <Navbar />
-            <Outlet />
+            <div className="min-h-[calc(100vh-178px)]">
+                <Outlet />
+            </div>
+            <Footer />
         </>
     );
 }
---
+++ client/src/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-indent */
+import { Typography } from '@mui/material';
+import React from 'react';
+
+function Footer() {
+    return (
+        <Typography variant="h6" className="text-gray-500 text-center py-5">
+            ©️ Ujjal Kumar Roy || All Right Reserved!!
+        </Typography>
+    );
+}
+
+export default Footer;
